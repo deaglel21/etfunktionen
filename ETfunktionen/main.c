332426d6dd8d298b95cd2459b9b17b4003f19d79
Arthur Lerke <arthur@arthurs-air.intern>
2017-05-13 10:01:51 +0200

    Initial Commit

---
+++ ETfunktionen/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ETfunktionen
+//
+//  Created by Arthur Lerke on 13.05.17.
+//  Copyright © 2017 Arthur Lerke. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
